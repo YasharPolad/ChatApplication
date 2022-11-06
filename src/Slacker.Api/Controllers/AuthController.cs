a163c27d85b012c5a046aeeb47613b4b0a9072ea
YasharPolad <yxchronos@gmail.com>
2022-11-06 02:26:45 -0800

    Basic registration without user roles

---
+++ src/Slacker.Api/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Slacker.Api.Contracts;
+using Slacker.Api.Contracts.User.Request;
+using Slacker.Application.Users.Commands;
+
+namespace Slacker.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
+
+    public AuthController(IMapper mapper, IMediator mediator)
+    {
+        _mapper = mapper;
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterRequest register)
+    {
+        if(!ModelState.IsValid)
+        {
+            //TODO: Turn model state error into our restful format
+            
+            var result = new ErrorResponse();
+            ModelState.ToList().ForEach(error => result.Errors.Add(error.Value.ToString()));
+            return BadRequest(result);
+
+               
+        }
+        var command = _mapper.Map<RegisterRequestCommand>(register);
+        var response = await _mediator.Send(command);
+
+        if (response.IsSuccess) 
+            return Ok();
+        else
+            return BadRequest(_mapper.Map<ErrorResponse>(response)); //TODO: Automatically return different error responses depending on the error code
+        
+    }
+}
---
+++ src/Slacker.Api/Mappings/UserMaps.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Slacker.Api.Contracts;
+using Slacker.Api.Contracts.User.Request;
+using Slacker.Application.Models.User;
+using Slacker.Application.Users.Commands;
+
+namespace Slacker.Api.Mappings;
+
+public class UserMaps : Profile
+{
+	public UserMaps()
+	{
+		CreateMap<RegisterRequest, RegisterRequestCommand>();
+		CreateMap<RegisterResponse, ErrorResponse>();
+	}
+}
