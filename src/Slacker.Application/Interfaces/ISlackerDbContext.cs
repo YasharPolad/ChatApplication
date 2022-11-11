cfd94efab695935da57a1594913f3a197a357c2f
YasharPolad <yxchronos@gmail.com>
2022-11-11 04:10:00 -0800

    Generic Repository set up. Entity class made abstract

--- src/Slacker.Application/Interfaces/ISlackerDbContext.cs
+++ src/Slacker.Application/Interfaces/ISlackerDbContext.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Slacker.Application.Interfaces;
-public interface ISlackerDbContext
+public interface ISlackerDbContext //TODO: Shouldn't the interface be ORM-agnostic? Should work with EF, dapper, or other orm.
 {
     public DbSet<Connection> Connections { get; }
     public DbSet<Employee> Employees { get; }
---
+++ src/Slacker.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Slacker.Application.Interfaces.RepositoryInterfaces;
+using Slacker.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slacker.Infrastructure.Repositories;
+public class GenericRepository<TEntity, TContext> : IGenericRepository<TEntity>
+    where TEntity : Entity, new()
+    where TContext : DbContext
+{
+    protected readonly TContext _context;
+
+    public GenericRepository(TContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(TEntity entity)
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(TEntity entity)
+    {
+        var entityToDelete = await GetAsync(e => e.Id == entity.Id);
+        _context.Set<TEntity>().Remove(entityToDelete);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<TEntity>?> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null)
+    {
+        if(predicate == null)
+            return await _context.Set<TEntity>().ToListAsync();
+        return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+    }
+
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        return await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        var entityToUpdate = await GetAsync(e => e.Id == entity.Id);
+        entityToUpdate = entity;
+        _context.Set<TEntity>().Update(entityToUpdate);
+        await _context.SaveChangesAsync();
+    }
+}
