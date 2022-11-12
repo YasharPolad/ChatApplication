56d9691d54a06132c492c7a26f1efbe59854475d
YasharPolad <yxchronos@gmail.com>
2022-11-12 11:23:45 -0800

    Get channels and get direct messages by employee

---
+++ src/Slacker.Application/Connections/Queries/GetChannelsByEmployeeQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Slacker.Application.Models;
+using Slacker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slacker.Application.Connections.Queries;
+public class GetChannelsByEmployeeQuery : IRequest<MediatrResult<List<Connection>>>
+{
+    public int EmployeeId { get; set; }
+}
