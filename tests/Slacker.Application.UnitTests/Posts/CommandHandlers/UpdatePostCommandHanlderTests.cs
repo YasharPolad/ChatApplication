d509182cc04e3780fff57cbad0ac306d6aca6355
YasharPolad <yxchronos@gmail.com>
2022-11-30 04:16:44 -0800

    update post tests

--- tests/Slacker.Application.UnitTests/Posts/CommandHandlers/UpdatePostCommandHanlderTests.cs
+++ tests/Slacker.Application.UnitTests/Posts/CommandHandlers/UpdatePostCommandHanlderTests.cs
@@ -1,4 +1,7 @@
-﻿using Shouldly;
+﻿using Moq;
+using Slacker.Application.Interfaces.RepositoryInterfaces;
+using Slacker.Application.Posts.CommandHandlers;
+using Slacker.Application.Posts.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +11,69 @@ using System.Threading.Tasks;
 namespace Slacker.Application.UnitTests.Posts.CommandHandlers;
 public class UpdatePostCommandHanlderTests
 {
+    private readonly Mock<IPostRepository> _postRepositoryMock;
+	UpdatePostCommand command;
+	public UpdatePostCommandHanlderTests()
+	{
+		_postRepositoryMock = new();
+		command = new UpdatePostCommand
+		{
+			postId = 15,
+			updatedMessage = "Updated test message"
+		};
+	}
+	
+	[Fact]
+	public async Task Handle_Should_ReturnFail_IfPostIsntFound()
+	{
+		//Arrange
+		_postRepositoryMock
+			.Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Post, bool>>>()))
+			.ReturnsAsync((Post)null);
+
+		var handler = new UpdatePostCommandHandler(_postRepositoryMock.Object);
+
+		//Act
+		var result = await handler.Handle(command, default);
+
+		//Assert
+		result.IsSuccess.ShouldBeFalse();
+		result.Errors.ShouldContain("This post doesn't exist");
+	}
+
+	[Fact]
+	public async Task Handle_Should_ReturnSucess_IfPostIsFound()
+	{
+		//Arrange
+		var postFake = new Post();
+        _postRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Post, bool>>>()))
+            .ReturnsAsync(postFake);
+		var handler = new UpdatePostCommandHandler(_postRepositoryMock.Object);
+
+		//Act
+		var result = await handler.Handle(command, default);
+
+        //Assert
+		result.IsSuccess.ShouldBeTrue();
+        result.Errors.ShouldNotContain("This post doesn't exist");
+    }
+
     [Fact]
-    public void DemoTest()
+    public async Task Handle_Should_Return_UpdatedPost_IfPostIsFound()
     {
-        string s = "hello";
-        s.ShouldBeOfType(typeof(string));   
+        //Arrange
+        var postFake = new Post();
+        _postRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Post, bool>>>()))
+            .ReturnsAsync(postFake);
+        var handler = new UpdatePostCommandHandler(_postRepositoryMock.Object);
+
+        //Act
+        var result = await handler.Handle(command, default);
+
+        //Assert
+        postFake.IsEdited.ShouldBeTrue();
+		postFake.Message.ShouldBeSameAs(command.updatedMessage);
     }
 }
--- tests/Slacker.Application.UnitTests/Usings.cs
+++ tests/Slacker.Application.UnitTests/Usings.cs
@@ -1,1 +1,6 @@
 global using Xunit;
+global using System.Linq.Expressions;
+global using Shouldly;
+global using Slacker.Domain.Entities;
+
+
