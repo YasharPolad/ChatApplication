d43d506a7a856c26564af4b743c710e0f4e242e8
YasharPolad <yxchronos@gmail.com>
2022-11-30 03:37:59 -0800

    move tests into test folder

---
+++ tests/Slacker.Application.UnitTests/Posts/CommandHandlers/UpdatePostCommandHanlderTests.cs
@@ -0,0 +1,17 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slacker.Application.UnitTests.Posts.CommandHandlers;
+public class UpdatePostCommandHanlderTests
+{
+    [Fact]
+    public void DemoTest()
+    {
+        string s = "hello";
+        s.ShouldBeOfType(typeof(string));   
+    }
+}
