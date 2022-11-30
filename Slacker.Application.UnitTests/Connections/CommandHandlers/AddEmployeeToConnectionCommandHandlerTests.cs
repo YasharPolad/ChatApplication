40ed1fd90540308c892d84e30e653038bd75d806
YasharPolad <yxchronos@gmail.com>
2022-11-30 02:22:45 -0800

    add employee to connection finished

--- Slacker.Application.UnitTests/Connections/CommandHandlers/AddEmployeeToConnectionCommandHandlerTests.cs
+++ Slacker.Application.UnitTests/Connections/CommandHandlers/AddEmployeeToConnectionCommandHandlerTests.cs
@@ -13,12 +13,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Slacker.Application.UnitTests.Connections.CommandHandlers;
-public class AddEmployeeToConnectionCommandHandlerTests : IDisposable
+public class AddEmployeeToConnectionCommandHandlerTests
 {
     private readonly Mock<IConnectionRepository> _connectionRepositoryMock;
     private readonly Mock<IEmployeeRepository> _employeeRepositoryMock;
     Mock<Employee> _employeeMock;
     AddEmployeeToConnectionCommand command;
+    List<Connection> _connections;
     public AddEmployeeToConnectionCommandHandlerTests()
     {
         _connectionRepositoryMock = new();
@@ -30,19 +31,31 @@ public class AddEmployeeToConnectionCommandHandlerTests : IDisposable
             EmployeeId = 1,
             ConnectionId = 2,
         };
+
+        _connections = new List<Connection>
+        {
+            new Connection  //Direct message that has one employee
+            {
+                IsChannel = false,
+                Employees = new List<Employee> {_employeeMock.Object}
+            },
+            new Connection //Direct message that has two employees already
+            {
+                IsChannel = false,
+                Employees = new List<Employee>{ _employeeMock.Object, _employeeMock.Object}
+            }
+        };
         
     }
-    public void Dispose()
-    {
-        command = null;
-    }
+
 
     [Fact]
     public async Task Handle_Should_ReturnError_WhenConnectionIsntFound()
     {
         //Arrange
         _connectionRepositoryMock
-            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>()))
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>(),
+                                         It.IsAny<Expression<Func<Connection, object>>>()))
             .ReturnsAsync((Connection)null);
 
         var handler = new AddEmployeeToConnectionCommandHandler(_connectionRepositoryMock.Object, _employeeRepositoryMock.Object);
@@ -57,19 +70,12 @@ public class AddEmployeeToConnectionCommandHandlerTests : IDisposable
 
     [Fact]
     public async Task Handle_Should_ReturnError_WhenConnectionIsDirectMessage_WithTwoEmployees()
-    {
-        var connection = new Connection();
-        connection.IsChannel = false;
-        connection.Employees = new List<Employee>
-        {
-            _employeeMock.Object,
-            _employeeMock.Object
-        };
-        Console.WriteLine(connection is null);
+    {        
         //Arrange           
         _connectionRepositoryMock
-            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>()))
-            .ReturnsAsync(connection);
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>(),
+                                         It.IsAny<Expression<Func<Connection, object>>>()))
+            .ReturnsAsync(_connections[1]);
 
         var handler = new AddEmployeeToConnectionCommandHandler(_connectionRepositoryMock.Object, _employeeRepositoryMock.Object);
 
@@ -81,21 +87,76 @@ public class AddEmployeeToConnectionCommandHandlerTests : IDisposable
         result.Errors.ShouldContain("Can't add a third employee to a direct message");
     }
 
-    //[Fact]
-    //public async Task Handle_Should_ReturnError_WhenConnectionIsntFound()
-    //{
-    //    //Arrange
-    //    _connectionRepositoryMock
-    //        .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>()))
-    //        .ReturnsAsync((Connection)null);
+    [Fact]
+    public async Task Handle_Should_ReturnError_WhenEmployeeIsntFound()
+    {
+        //Arrange
+        _connectionRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>(),
+                                         It.IsAny<Expression<Func<Connection, object>>>()))
+            .ReturnsAsync(_connections[0]);
 
-    //    var handler = new AddEmployeeToConnectionCommandHandler(_connectionRepositoryMock.Object, _employeeRepositoryMock.Object);
+        _employeeRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .ReturnsAsync((Employee)null);
 
-    //    //Act
-    //    var result = await handler.Handle(command, default);
+        var handler = new AddEmployeeToConnectionCommandHandler(_connectionRepositoryMock.Object, _employeeRepositoryMock.Object);
 
-    //    //Assert
-    //    result.IsSuccess.ShouldBe(false);
-    //    result.Errors.ShouldContain("The connection wasn't found");
-    //}
+        //Act
+        var result = await handler.Handle(command, default);
+
+        //Assert
+        result.IsSuccess.ShouldBe(false);
+        result.Errors.ShouldContain("The employee wasn't found");
+    }
+
+    [Fact]  
+    public async Task Handle_Should_ReturnError_IfEmployeeIsAlreadyInTheConnection()
+    {
+        //Arrange
+        _connectionRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>(),
+                                         It.IsAny<Expression<Func<Connection, object>>>()))
+            .ReturnsAsync(_connections[0]);
+
+        _employeeRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .ReturnsAsync(_employeeMock.Object);
+        
+        var handler = new AddEmployeeToConnectionCommandHandler(_connectionRepositoryMock.Object, _employeeRepositoryMock.Object);
+        
+        //Act
+        var result = await handler.Handle(command, default);
+
+        //Assert
+        result.IsSuccess.ShouldBe(false);
+        result.Errors.ShouldContain($"Employee {command.EmployeeId} is already in connection {command.ConnectionId}");
+
+
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnSuccess_IfAllOtherTestsPass()
+    {
+        //Arrange
+        _connectionRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Connection, bool>>>(),
+                                         It.IsAny<Expression<Func<Connection, object>>>()))
+            .ReturnsAsync(_connections[0]);
+        
+        var newEmployeeMock = new Mock<Employee>();
+
+        _employeeRepositoryMock
+            .Setup(repo => repo.GetAsync(It.IsAny<Expression<Func<Employee, bool>>>()))
+            .ReturnsAsync(newEmployeeMock.Object);
+
+        var handler = new AddEmployeeToConnectionCommandHandler(_connectionRepositoryMock.Object, _employeeRepositoryMock.Object);
+
+        //Act
+        var result = await handler.Handle(command, default);
+
+        //Assert
+        result.IsSuccess.ShouldBe(true);
+        _connections[0].Employees.ShouldContain(newEmployeeMock.Object);
+    }
 }
