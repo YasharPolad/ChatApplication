e595210c7ee7373141e3023da170770438b41c05
YasharPolad <yxchronos@gmail.com>
2022-12-08 12:09:10 -0800

    test file path made relative

--- tests/Slacker.Infrastructure.UnitTests/FileHandlerServiceTests.cs
+++ tests/Slacker.Infrastructure.UnitTests/FileHandlerServiceTests.cs
@@ -18,7 +18,7 @@ public class FileHandlerServiceTests : IDisposable
     public FileHandlerServiceTests()
     {
         _envMock = new();
-        baseDirectory = @"C:\Users\DELL\source\repos\Slacker\Slacker\src\Slacker.Api\wwwroot\Uploads\";
+        baseDirectory = @"..\..\src\Slacker.Api\wwwroot\Uploads";
 
         //Create test file
         var filePath = Path.Combine(baseDirectory, "fakeFile.txt");
